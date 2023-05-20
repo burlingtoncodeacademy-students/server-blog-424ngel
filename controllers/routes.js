664783636387928361d69a3814243c2aeebc2d46
424ngel <angel.aguilarestra@gmail.com>
2023-05-20 11:27:37 -0400

    Updated code to use fs (reads/writes directly from file)

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,20 +1,33 @@
 const express = require('express')
 const router = express.Router()
 const fs = require('fs')
+const path = require('path')
 
-const posts = require('../api/blog.json')
+const filePath = path.join(__dirname, '../api/blog.json') // concatenates paths to form single path
+// __dirname -> (Node.js variable) represents directory name 
+
+function readFile() {
+  const rawData = fs.readFileSync(filePath) // Reads the file synchronously
+  return JSON.parse(rawData) // Parses the raw data and converts it into a JS object
+}
+
+function writeFile(data) { // Writes data directly to file
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2)) // .stringify(data, null, 2) converts the data into a string with two spaces of indentation 
+}
 
 router.get('/all', (req, res) => { // Gets all posts
+  const posts = readFile()
   res.json(posts)
 })
 
 router.get('/comments', (req, res) => { // Returns an array of the bodies of each post
+  const posts = readFile()
   const comments = posts.map(post => post.body)
-
   res.json(comments)
 })
 
 router.get('/:id', (req, res) => { // Gets specific post by id
+  const posts = readFile()
   const postId = parseInt(req.params.id)
   const post = posts.find(post => post.post_id === postId) // Finds the post by the id that is entered
 
@@ -22,6 +35,7 @@ router.get('/:id', (req, res) => { // Gets specific post by id
 })
 
 router.post('/create', (req, res) => { // Creates new posts
+  const posts = readFile()
   const ids = posts.map(post => post.post_id) // Array of all the post ids
 
   // Adds 1 to the last post in the array's id to create a new id
@@ -31,15 +45,16 @@ router.post('/create', (req, res) => { // Creates new posts
   req.body.post_id = newId
   posts.push(req.body)
 
+  writeFile(posts)
   res.json({
-    message: 'created new post',
+    message: 'Created new post',
     newPost: req.body
   })
 })
 
 router.put('/update/:id', (req, res) => { // Updates post
   const postId = parseInt(req.params.id)
-
+  const posts = readFile()
   // Finds the index of the post in the array based on the post ID
   const postIndex = posts.findIndex(post => post.post_id === postId)
 
@@ -47,21 +62,23 @@ router.put('/update/:id', (req, res) => { // Updates post
   posts[postIndex] = req.body
   posts[postIndex].post_id = postId
   
-
+  writeFile(posts)
   res.json({
-    message: 'post updated successfully',
+    message: 'Post updated successfully',
     updatedPost: req.body
   })
 })
 
 router.delete('/delete/:id', (req, res) => { // Deletes post
   const postId = parseInt(req.params.id)
+  const posts = readFile()
   const postIndex = posts.findIndex(post => post.post_id === postId)
 
   const deletedPost = posts.splice(postIndex, 1) // .splice returns the deleted post, then stores it in the deletedPost variable
 
+  writeFile(posts)
   res.json({
-    message: 'deletion successful',
+    message: 'Deletion successful',
     postDeleted: deletedPost
   })
 })
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-
+require('dotenv').config()
+const PORT = process.env.PORT
 const router = require('./controllers/routes')
 
-const PORT = 4004
 
 app.use(express.json())
 app.use(router)
